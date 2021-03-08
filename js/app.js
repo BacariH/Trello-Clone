5c68dcacbc9fd27fc44d64cd3dd38adee652af46
Noah Glaser <glaserpower@gmail.com>
2021-03-07 18:18:34 -0800

    put the event listener on the main element and filter with an if statement

--- js/app.js
+++ js/app.js
@@ -1,69 +1,62 @@
-const createBtn = document.querySelector('button.createBtn');
+const createBtn = document.querySelector("button.createBtn");
 let indexValue = 0;
-function createTodo(){
-    let div = document.querySelector("#placement-div");
-    let todo = document.querySelector('textarea');
-    let newDiv = document.createElement('div');
-    let moveBtn = document.createElement('button');
+function createTodo() {
+  let div = document.querySelector("#placement-div");
+  let todo = document.querySelector("textarea");
+  let newDiv = document.createElement("div");
+  let moveBtn = document.createElement("button");
 
-    if(todo.value === ""){
-        alert("Invalid");
-    } else{
-        
-        newDiv.classList = "bg-opacity-20 rounded-lg bg-gray-100 flex flex-col";
-        moveBtn.classList = "w-4/12 border-black border rounded-full moveBtn";
-        moveBtn.innerText = "Move";
-        newDiv.innerHTML = `
+  if (todo.value === "") {
+    alert("Invalid");
+  } else {
+    newDiv.classList = "bg-opacity-20 rounded-lg bg-gray-100 flex flex-col";
+    moveBtn.classList = "w-4/12 border-black border rounded-full moveBtn";
+    moveBtn.innerText = "Move";
+    newDiv.innerHTML = `
         <p class="p-3">${todo.value}</p>
         `;
-        moveBtn.setAttribute("onclick", "moveProgress()")
-        moveBtn.parentNode = newDiv;
-        newDiv.parentNode = div;
-    
-        newDiv.appendChild(moveBtn);
-        div.appendChild(newDiv);
-        todo.value = "";
+    moveBtn.parentNode = newDiv;
+    newDiv.parentNode = div;
+
+    newDiv.appendChild(moveBtn);
+    div.appendChild(newDiv);
+    todo.value = "";
+  }
+}
+
+// This is all your original logic
+function moveCard(btn) {
+  console.log(btn.previousElementSibling);
+  //gets the button that was created in the create todo method
+  //is the parent div of btn
+  let todo = btn.parentNode;
+  //is the parent column of the todo
+  let parent = todo.parentNode;
+  if (todo.id === "last") {
+    let closeBtn = document.createElement("button");
+    closeBtn.innerText = "Close";
+    todo = closeBtn.parentNode;
+    todo.appendChild(closeBtn);
+  }
+
+  const todoDiv = document.querySelectorAll(".todo-div");
+
+  let found;
+
+  todoDiv.forEach((div, index) => {
+    if (parent === div) {
+      found = index + 1;
     }
-    
-    
+  });
+  todoDiv[found].appendChild(todo);
 }
 
-function moveProgress(btn){
-    console.log(btn);
-
-    // //TODO edit make sure the click eventhandler first click does not double click.
-    // //it does a double click
-    // moveBtns.forEach((btn) => {
-    //     btn.addEventListener('click', () => {
-            
-    //         console.log(btn.previousElementSibling);
-    //         //gets the button that was created in the create todo method
-    //         //is the parent div of btn
-    //         let todo = btn.parentNode;
-    //         //is the parent column of the todo
-    //         let parent = todo.parentNode;
-    //         if(todo.id === "last" ){
-    //             let closeBtn = document.createElement('button');
-    //             closeBtn.innerText = 'Close';
-    //             todo = closeBtn.parentNode;
-    //             todo.appendChild(closeBtn);
-    //         }
-        
-    //         const todoDiv = document.querySelectorAll('.todo-div');
-            
-    //         let found;
-            
-        
-    //         todoDiv.forEach((div, index) => {
-    //             if(parent === div){
-    //                 found = index + 1;
-    //             } 
-                
-    //         })
-    //         todoDiv[found].appendChild(todo);
-            
-    //     });
-    // })
-   
-}
-
+const main = document.querySelector("main");
+// This event listener only responds to move events
+main.addEventListener("click", (e) => {
+  // If we are not clicking a move button than don't do anything.
+  if (!e.target.classList.contains("moveBtn")) {
+    return;
+  }
+  moveCard(e.target);
+});
